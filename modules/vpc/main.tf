ecf66ab0eb46035e15c0c386ec982a890fad7bd4
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-02-20 18:00:09 -0500

    setup for packer

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -24,3 +24,85 @@ resource "google_compute_route" "default_route" {
   next_hop_gateway = "default-internet-gateway"
   depends_on       = [google_compute_network.vpc]
 }
+
+
+resource "google_compute_instance" "compute-1-csye6225" {
+  boot_disk {
+    device_name = "compute-1-csye6225"
+
+    initialize_params {
+      image = "projects/tf-project-csye-6225/global/images/custom-image-with-mysql"
+      size  = 100
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+  labels = {
+    goog-ec-src = "vm_add-tf"
+  }
+
+  machine_type = "custom-1-1024"
+  name         = "compute-1-csye6225"
+  project      = var.project
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork = google_compute_subnetwork.subnet[0].name
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    email  = "csye6225-service-for-packer@tf-project-csye-6225.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "us-east1-b"
+  depends_on = [google_compute_subnetwork.subnet[0]]
+}
+
+# Set up firewall rules
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc.name
+  project = var.project
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+  depends_on = [google_compute_network.vpc]
+}
+
+resource "google_compute_firewall" "deny_ssh" {
+  name    = "deny-ssh"
+  network = google_compute_network.vpc.name
+  project = var.project
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"] 
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  depends_on = [google_compute_network.vpc]
+}
