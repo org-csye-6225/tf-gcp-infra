a9bb600b688b71d7cf7073fe859703e890913ac4
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-03-12 15:50:32 -0400

    test

--- dev-infra-gcp/variables.tf
+++ dev-infra-gcp/variables.tf
@@ -19,7 +19,31 @@ variable "public_subnet" {
 
 variable "credentials_file" {
 }
-
 variable "vpcs" {
     description = "A map of VPC configurations."
 }
+
+variable "deletion_protection"{
+  type = bool
+  default = false
+}
+variable "availability_type" {
+  type        = string
+  default     = "REGIONAL"
+}
+
+variable "disk_type" {
+  type        = string
+  default     = "pd-ssd"
+}
+
+variable "disk_size" {
+  type        = number
+  default     = 100
+}
+
+variable "ipv4_enabled" {
+  type        = bool
+  default     = false
+}
+
