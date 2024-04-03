0ae17021e6ea619e5b9afda0927b1099055d2c3d
Abhinav-NEU <abhinavp.klg@gmail.com>
2024-04-02 22:36:34 -0400

    lb1

--- dev-infra-gcp/modules/vpc/main.tf
+++ dev-infra-gcp/modules/vpc/main.tf
@@ -39,7 +39,6 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.self_link
   depends_on    = [google_compute_network.vpc]
   private_ip_google_access = true
-  # private_ip_google_access = (count.index == 1) ? 1 : 0
 }
 
 resource "google_compute_route" "default_route" {
@@ -121,92 +120,91 @@ resource "random_string" "auth_pswd" {
   special = false
 }
 
-resource "google_compute_instance" "compute-csye6225" {
-  boot_disk {
-    device_name = "compute-csye6225"
+# resource "google_compute_instance" "compute-csye6225" {
+#   boot_disk {
+#     device_name = "compute-csye6225"
 
-    initialize_params {
-      image = "projects/tf-csye-6225-project/global/images/custom-image-with-pubsub-1711567402"
-      size  = 100
-      type  = "pd-balanced"
-    }
+#     initialize_params {
+#       image = "projects/tf-csye-6225-project/global/images/custom-image-with-pubsub-1711567402"
+#       size  = 100
+#       type  = "pd-balanced"
+#     }
 
-    mode = "READ_WRITE"
-  }
+#     mode = "READ_WRITE"
+#   }
 
-  machine_type = "e2-standard-4"
-  name         = "compute-csye6225"
-  project      = var.project
+#   machine_type = "e2-standard-4"
+#   name         = "compute-csye6225"
+#   project      = var.project
 
-   network_interface {
-    access_config {
-      network_tier = "PREMIUM"
-    }
-    subnetwork = google_compute_subnetwork.subnet[0].self_link
-  }
- metadata = {
-  startup-script = <<-EOT
-    #!/bin/bash
-    cat <<EOF >> /opt/csye6225/webapp/.env
-    DATABASE=webapp
-    SQL_USER=webapp
-    SQL_PSWD=${random_string.database_pswd.result}
-    AUTH_USER=test@test.com
-    AUTH_PSWD=${random_string.auth_pswd.result}
-    HOST=${google_sql_database_instance.db_instance_10.private_ip_address}
-    EOF
-    chown csye6225:csye6225 /opt/csye6225/webapp/.env
+#    network_interface {
+#     access_config {
+#       network_tier = "PREMIUM"
+#     }
+#     subnetwork = google_compute_subnetwork.subnet[0].self_link
+#   }
+#  metadata = {
+#   startup-script = <<-EOT
+#     #!/bin/bash
+#     cat <<EOF >> /opt/csye6225/webapp/.env
+#     DATABASE=webapp
+#     SQL_USER=webapp
+#     SQL_PSWD=${random_string.database_pswd.result}
+#     AUTH_USER=test@test.com
+#     AUTH_PSWD=${random_string.auth_pswd.result}
+#     HOST=${google_sql_database_instance.db_instance_10.private_ip_address}
+#     EOF
+#     chown csye6225:csye6225 /opt/csye6225/webapp/.env
 
-    # Create or update config.yaml
-    cat <<EOF > /etc/google-cloud-ops-agent/config.yaml
-logging:
- receivers:
-   my-app-receiver:
-     type: files
-     include_paths:
-       - /var/log/webapp/combined.log
-     record_log_file_path: true
- processors:
-   my-app-processor:
-     type: parse_json
-     time_key: time
-     time_format: "%Y-%m-%dT%H:%M:%S.%fZ"
-   move_severity:
-     type: modify_fields
-     fields:
-       severity:
-         move_from: jsonPayload.level
-         map_values:
-           INFO: "info"
-           ERROR: "error"
-           WARNING: "warn"
-           DEBUG: "debug"
- service:
-   pipelines:
-     default_pipeline:
-       receivers: [my-app-receiver]
-       processors: [my-app-processor, move_severity]
-EOF
+#     # Create or update config.yaml
+#     cat <<EOF > /etc/google-cloud-ops-agent/config.yaml
+# logging:
+#  receivers:
+#    my-app-receiver:
+#      type: files
+#      include_paths:
+#        - /var/log/webapp/combined.log
+#      record_log_file_path: true
+#  processors:
+#    my-app-processor:
+#      type: parse_json
+#      time_key: time
+#      time_format: "%Y-%m-%dT%H:%M:%S.%fZ"
+#    move_severity:
+#      type: modify_fields
+#      fields:
+#        severity:
+#          move_from: jsonPayload.level
+#          map_values:
+#            INFO: "info"
+#            ERROR: "error"
+#            WARNING: "warn"
+#            DEBUG: "debug"
+#  service:
+#    pipelines:
+#      default_pipeline:
+#        receivers: [my-app-receiver]
+#        processors: [my-app-processor, move_severity]
+# EOF
 
-    # Restart google-cloud-ops-agent service
-    sudo systemctl restart google-cloud-ops-agent
-  EOT
-}
+#     # Restart google-cloud-ops-agent service
+#     sudo systemctl restart google-cloud-ops-agent
+#   EOT
+# }
 
-  service_account {
-       email  = google_service_account.my_service_account.email
-       scopes = ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/pubsub"]
-  }
+#   service_account {
+#        email  = google_service_account.my_service_account.email
+#        scopes = ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/pubsub"]
+#   }
 
-  zone = "us-east1-b"
-  depends_on = [
-    google_sql_database_instance.db_instance_10,
-    google_service_account.my_service_account,
-    google_project_iam_binding.logging_admin_binding,
-    google_project_iam_binding.monitoring_metric_writer_binding
-  ]
-}
-
+#   zone = "us-east1-b"
+#   depends_on = [
+#     google_sql_database_instance.db_instance_10,
+#     google_service_account.my_service_account,
+#     google_project_iam_binding.logging_admin_binding,
+#     google_project_iam_binding.monitoring_metric_writer_binding
+#   ]
+# }
 resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
   network = google_compute_network.vpc.self_link
@@ -221,32 +219,32 @@ resource "google_compute_firewall" "allow_http" {
   depends_on = [google_compute_network.vpc]
 }
 
-resource "google_compute_firewall" "deny_ssh" {
-  name    = "deny-ssh"
-  network = google_compute_network.vpc.self_link
-  project = var.project
+# resource "google_compute_firewall" "deny_ssh" {
+#   name    = "deny-ssh"
+#   network = google_compute_network.vpc.self_link
+#   project = var.project
 
-  deny {
-    protocol = "tcp"
-    ports    = ["22"] 
-  }
+#   deny {
+#     protocol = "tcp"
+#     ports    = ["22"] 
+#   }
 
-  source_ranges = ["0.0.0.0/0"]
-  depends_on = [google_compute_network.vpc]
-}
+#   source_ranges = ["0.0.0.0/0"]
+#   depends_on = [google_compute_network.vpc]
+# }
 
 
-resource "google_dns_record_set" "dns_record" {
-  name         = "abhinavpandey.tech."
-  type         = "A"
-  ttl          = 360
-  managed_zone = "abhinav-csye6225"
+# resource "google_dns_record_set" "dns_record" {
+#   name         = "abhinavpandey.tech."
+#   type         = "A"
+#   ttl          = 360
+#   managed_zone = "abhinav-csye6225"
 
-  rrdatas = [
-    google_compute_instance.compute-csye6225.network_interface[0].access_config[0].nat_ip
-  ]
-  depends_on = [google_compute_instance.compute-csye6225]
-}
+#   rrdatas = [
+#     google_compute_region_instance_template.compute-csye6225.network_interface[0].access_config[0].nat_ip
+#   ]
+#   depends_on = [google_compute_region_instance_template.compute-csye6225]
+# }
 resource "google_dns_record_set" "mx_record" {
   name         = "abhinavpandey.tech."
   type         = "MX"
@@ -257,7 +255,7 @@ resource "google_dns_record_set" "mx_record" {
     "10 mxa.mailgun.org.",
     "10 mxb.mailgun.org."
   ]
-  depends_on = [google_compute_instance.compute-csye6225]
+  depends_on = [google_compute_region_instance_template.compute-csye6225]
 }
 
 resource "google_dns_record_set" "txt_record" {
@@ -269,7 +267,7 @@ resource "google_dns_record_set" "txt_record" {
   rrdatas = [
     "\"v=spf1 include:mailgun.org ~all\""
   ]
-  depends_on = [google_compute_instance.compute-csye6225]
+  depends_on = [google_compute_region_instance_template.compute-csye6225]
 }
 
 resource "google_dns_record_set" "cname_record" {
@@ -281,7 +279,7 @@ resource "google_dns_record_set" "cname_record" {
   rrdatas = [
     "mailgun.org."
   ]
-  depends_on = [google_compute_instance.compute-csye6225]
+  depends_on = [google_compute_region_instance_template.compute-csye6225]
 }
 resource "google_dns_record_set" "txt_domainkey_record" {
   name         = "pic._domainkey.abhinavpandey.tech."
@@ -292,7 +290,7 @@ resource "google_dns_record_set" "txt_domainkey_record" {
   rrdatas = [
     var.domain_key_record
   ]
-  depends_on = [google_compute_instance.compute-csye6225]
+  depends_on = [google_compute_region_instance_template.compute-csye6225]
 }
 resource "google_vpc_access_connector" "connector" {
   name          = "vpc-connector"
@@ -384,7 +382,7 @@ resource "google_cloudfunctions2_function" "email_function" {
       DB_HOST     = google_sql_database_instance.db_instance_10.private_ip_address
       INSTANCE    = google_sql_database_instance.db_instance_10.name
       API_KEY     = var.api_key_mailgun
-      IP          = google_compute_instance.compute-csye6225.network_interface[0].access_config[0].nat_ip
+      IP          = google_compute_region_instance_template.compute-csye6225.network_interface[0].access_config[0].nat_ip
     }
     service_account_email = google_service_account.cloud_function_SA.email
     vpc_connector         = google_vpc_access_connector.connector.self_link
@@ -400,5 +398,203 @@ resource "google_cloudfunctions2_function" "email_function" {
 
   depends_on = [
     google_vpc_access_connector.connector
+  ]
+}
+
+## load balancer related code here: 
+resource "google_compute_firewall" "allow_lb" {
+  name    = "allow-lb"
+  network = google_compute_network.vpc.self_link
+  project = var.project
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+  depends_on = [google_compute_network.vpc]
+}
+resource "google_compute_region_instance_template" "compute-csye6225" {
+  name        = "my-instance-template"
+  machine_type = "e2-standard-4"
+  project     = var.project
+
+  disk {
+    source_image = "projects/tf-csye-6225-project/global/images/custom-image-with-pubsub-1711567402"
+    disk_size_gb  = 100
+    type  = "pd-balanced"
+    auto_delete = true
+    boot        = true
+  }
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+    subnetwork = google_compute_subnetwork.subnet[0].self_link
+  }
+
+ metadata = {
+  startup-script = <<-EOT
+    #!/bin/bash
+    cat <<EOF >> /opt/csye6225/webapp/.env
+    DATABASE=webapp
+    SQL_USER=webapp
+    SQL_PSWD=${random_string.database_pswd.result}
+    AUTH_USER=test@test.com
+    AUTH_PSWD=${random_string.auth_pswd.result}
+    HOST=${google_sql_database_instance.db_instance_10.private_ip_address}
+    EOF
+    chown csye6225:csye6225 /opt/csye6225/webapp/.env
+
+    # Create or update config.yaml
+    cat <<EOF > /etc/google-cloud-ops-agent/config.yaml
+logging:
+ receivers:
+   my-app-receiver:
+     type: files
+     include_paths:
+       - /var/log/webapp/combined.log
+     record_log_file_path: true
+ processors:
+   my-app-processor:
+     type: parse_json
+     time_key: time
+     time_format: "%Y-%m-%dT%H:%M:%S.%fZ"
+   move_severity:
+     type: modify_fields
+     fields:
+       severity:
+         move_from: jsonPayload.level
+         map_values:
+           INFO: "info"
+           ERROR: "error"
+           WARNING: "warn"
+           DEBUG: "debug"
+ service:
+   pipelines:
+     default_pipeline:
+       receivers: [my-app-receiver]
+       processors: [my-app-processor, move_severity]
+EOF
+
+    # Restart google-cloud-ops-agent service
+    sudo systemctl restart google-cloud-ops-agent
+  EOT
+}
+depends_on = [google_sql_database_instance.db_instance_10]
+}
+resource "google_compute_https_health_check" "health_check" {
+  name               = "health-check"
+  request_path       = "/healthz"
+  port               = 3000
+  check_interval_sec = 40
+  timeout_sec        = 30
+  healthy_threshold  = 6
+  unhealthy_threshold = 6
+  depends_on = [ google_compute_region_instance_template.compute-csye6225]
+}
+
+resource "google_compute_region_autoscaler" "my_autoscaler" {
+  name               = "my-autoscaler"
+  region             = var.region
+  target             = google_compute_region_instance_group_manager.my_instance_group.id
+  autoscaling_policy {
+    min_replicas      = 1
+    max_replicas      = 2
+    cpu_utilization {
+      target = 0.5
+    }
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "my_instance_group" {
+  name               = "my-instance-group"
+  base_instance_name = "my-instance"
+  project            = var.project
+  region             = var.region
+  distribution_policy_zones = [ "us-east1-b","us-east1-c","us-east1-d"]
+
+  target_size        = 3
+
+auto_healing_policies {
+    health_check = google_compute_https_health_check.health_check.id
+    initial_delay_sec = 60
+  }
+   version {
+    name = "instance-template-version"
+    instance_template = google_compute_region_instance_template.compute-csye6225.self_link
+  }
+
+  named_port {
+    name = "https"
+    port = 3000
+  }
+  depends_on  = [google_compute_region_instance_template.compute-csye6225]
+}
+
+resource "google_compute_global_address" "lb_ipv4_addr" {
+  name = "lb-ipv4-addr"
+}
+
+#           self managed SSL certificate
+
+# resource "google_compute_ssl_certificate" "ssl_cert" {
+#   name_prefix = "my-certificate-"
+#   private_key = file("path/to/your/private-key.pem")
+#   certificate = file("path/to/your/certificate.pem")
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+resource "google_compute_managed_ssl_certificate" "managed_cert" {
+  name = "managed-cert"
+
+  managed {
+    domains = ["abhinavpandey.tech"]
+  }
+}
+
+resource "google_compute_target_https_proxy" "https_proxy" {
+  name             = "https-proxy"
+  ssl_certificates = [google_compute_managed_ssl_certificate.managed_cert.self_link]
+  url_map          = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
+}
+
+resource "google_compute_backend_service" "backend_service" {
+  name                  = "backend-service"
+  protocol              = "HTTPS"
+  port_name             = "https"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  health_checks         = [google_compute_https_health_check.health_check.self_link]
+  locality_lb_policy    = "ROUND_ROBIN"
+  backend {
+    group = google_compute_region_instance_group_manager.my_instance_group.instance_group
+  }
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name                  = "forwarding-rule"
+  target                = google_compute_target_https_proxy.https_proxy.self_link
+  ip_address            = google_compute_global_address.lb_ipv4_addr.address
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "443"  
+}
+
+resource "google_dns_record_set" "dns_record" {
+  name         = "abhinavpandey.tech."
+  type         = "A"
+  ttl          = 360
+  managed_zone = "abhinav-csye6225"
+
+  rrdatas = [
+    google_compute_global_address.lb_ipv4_addr.address
   ]
 }
